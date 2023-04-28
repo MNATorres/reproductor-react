d4246944c1cef52f1de15b9d48a913bf593c782f
MNATorres <mtri21019@gmail.com>
2023-04-28 18:19:23 -0300

    se agrega funcion handlePrev

--- src/components/controlls/Controlls.tsx
+++ src/components/controlls/Controlls.tsx
@@ -4,11 +4,11 @@ import { MusicContext } from '../../context/GlobalContext';
 import { useContext } from 'react';
 
 export default function Controlls() {
-const {handleNext} = useContext(MusicContext)
+const {handleNext, handlePrev} = useContext(MusicContext)
 
   return (
     <Box sx={{ display: 'flex', width: '100%', justifyContent: 'space-evenly', marginTop: 5}}>
-        <ButtonController handler={handleNext} nameButton='Prev' />
+        <ButtonController handler={handlePrev} nameButton='Prev' />
         <ButtonController handler={handleNext} nameButton='Mode' />
         <ButtonController handler={handleNext} nameButton='Next' />
     </Box>
--- src/context/GlobalContext.tsx
+++ src/context/GlobalContext.tsx
@@ -4,26 +4,43 @@ export const music = ["Asturias", "Cambalache", "Azteca", "Electro", "Recuerdos"
 
 interface Music {
     handleNext: () => void
-    currentSong: string
+    handlePrev: () => void
+    currentSong: string | null
 }
 
 export const MusicContext = React.createContext<Music>({
-    handleNext: () => {},
+    handleNext: () => { },
+    handlePrev: () => { },
     currentSong: ""
 });
 
 export const MusicProvider = ({ children }: { children: React.ReactNode }) => {
-const [nextSong, setNextSong] = useState(0)
-const [currentSong, setCurrentSong] = useState(music[0]);
+    const [song, setSong] = useState(0)
+    const [currentSong, setCurrentSong] = useState<null | string>(music[0]);
 
 
-const handleNext = () => {
-setNextSong((nextSong + 1) % music.length);
-setCurrentSong(music[nextSong])
-}
+
+    const handleNext = () => {
+        const nextSong = (song + 1) % music.length;
+        setCurrentSong(music[nextSong]);
+        setSong(nextSong);
+        console.log(nextSong)
+      };
+
+    const handlePrev = () => {
+        let prevSong = song - 1;
+        if (prevSong < 0) {
+            prevSong = music.length - 1;
+        }
+        setSong(prevSong);
+        setCurrentSong(music[prevSong]);
+        console.log(prevSong)
+    };
+
+
 
     return (
-        <MusicContext.Provider value={{handleNext, currentSong}}>
+        <MusicContext.Provider value={{ handleNext, currentSong, handlePrev }}>
             {children}
         </MusicContext.Provider>
     )
